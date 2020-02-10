354d718da38fcb006f52adb544804c4c6eca0556
김찬란 <seriousran@gmail.com>
2020-02-10 17:38:42 +0900

    add time

--- main.go
+++ main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"github.com/JesusIslam/tldr"
+  "fmt"
+  "time"
+  "log"
+  "io/ioutil"
+  "github.com/JesusIslam/tldr"
 )
 
 func main() {
-	intoSentences := 3
-	textB, _ := ioutil.ReadFile("./input/sample.txt")
-	text := string(textB)
-	bag := tldr.New()
-	result, _ := bag.Summarize(text, intoSentences)
-	fmt.Println(result)
+  intoSentences := 3
+
+  start := time.Now()
+  
+  textB, _ := ioutil.ReadFile("./input/sample.txt")
+  text := string(textB)
+  bag := tldr.New()
+  result, _ := bag.Summarize(text, intoSentences)
+  fmt.Println(result)
+  
+  elapsed := time.Since(start)
+  log.Printf("It took %s", elapsed)
 }
